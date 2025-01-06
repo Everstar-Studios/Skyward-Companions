eca735654b94c05496cd9bb6c033d01b18bead93
omrkocar <kocartahir@gmail.com>
2025-01-06 13:43:47 -0500

    Added Camera and GameInput systems

---
+++ Skyward_Project/Assets/Scripts/Systems/GameInputSystem.cs
@@ -0,0 +1,53 @@
+using System;
+using Skyward.Core;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Skyward.Systems
+{
+    public class GameInputSystem : BaseSystem<GameInputSystem>
+    {
+        [SerializeField]
+        private PlayerInput playerInput;
+
+        private static PlayerInput PlayerInput => Instance.playerInput;
+        
+        // TODO OK: Implement generic commands for input actions
+        public static event Action<Vector2> onMove;
+        public static event Action<Vector2> onLook;
+        public static event Action onInteract;
+        public static event Action onJump;
+        public static event Action<bool> onSprint;
+        public static event Action onTogglePOV;
+
+        public void OnMove(InputValue value)
+        {
+            onMove?.Invoke(value.Get<Vector2>());
+        }
+
+        public void OnLook(InputValue value)
+        {
+            onLook?.Invoke(value.Get<Vector2>());
+        }
+
+        public void OnInteract(InputValue value)
+        {
+            onInteract?.Invoke();
+        }
+
+        public void OnJump(InputValue value)
+        {
+            onJump?.Invoke();
+        }
+
+        public void OnSprint(InputValue value)
+        {
+            onSprint?.Invoke(value.isPressed);
+        }
+
+        public void OnTogglePOV(InputValue value)
+        {
+            onTogglePOV?.Invoke();
+        }
+    }
+}
