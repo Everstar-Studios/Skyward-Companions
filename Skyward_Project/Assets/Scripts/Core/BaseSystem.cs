9c5f5d43c23e2e2a94ba5aeb548745d666b729b8
omrkocar <kocartahir@gmail.com>
2025-01-06 13:22:12 -0500

    Created Base System

---
+++ Skyward_Project/Assets/Scripts/Core/BaseSystem.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace Skyward.Core
+{
+    public abstract class BaseSystem : MonoBehaviour, ISystem
+    {
+        protected GameContext GameContext { get; private set; }
+        
+        protected virtual void Preload(GameContext context) { }
+        protected virtual void Initialize(GameContext context) { }
+        protected virtual void Cleanup() { }
+
+        void ISystem.Preload(GameContext context)
+        {
+            Preload(context);
+        }
+        
+        void ISystem.Initialize(GameContext context)
+        {
+            GameContext = context;
+            
+            Initialize(context);
+        }
+
+        void ISystem.Cleanup()
+        {
+            Cleanup();
+        }
+    }
+
+    public abstract class BaseSystem<T> : BaseSystem where T : BaseSystem
+    {
+        public static T Instance { get; private set; }
+
+        protected virtual void Awake()
+        {
+            if (Instance != null && Instance != this)
+                Destroy(gameObject);
+            else
+                Instance = this as T;
+            
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+}
