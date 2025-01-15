a604ffcb0daf35664a6f4547d7810fae5ffd3820
omrkocar <kocartahir@gmail.com>
2025-01-14 19:41:27 -0500

    GameInputSystem is in the same layer as player input

--- Skyward_Project/Assets/Scripts/Systems/GameInputSystem.cs
+++ Skyward_Project/Assets/Scripts/Systems/GameInputSystem.cs
@@ -7,13 +7,9 @@ namespace Skyward.Systems
 {
     public class GameInputSystem : BaseSystem<GameInputSystem>
     {
-        private static PlayerInput playerInput;
+        private PlayerInput playerInput;
 
-        public static PlayerInput PlayerInput
-        {
-            get => playerInput;
-            set => playerInput = value;
-        }
+        public static PlayerInput PlayerInput => Instance.playerInput;
         
         // TODO OK: Implement generic commands for input actions
         public static event Action<Vector2> onMove;
@@ -23,14 +19,22 @@ namespace Skyward.Systems
         public static event Action<bool> onSprint;
         public static event Action onTogglePOV;
 
+        protected override void Awake()
+        {
+            base.Awake();
+
+            playerInput = GetComponent<PlayerInput>();
+        }
+
         public void OnMove(InputAction.CallbackContext context)
         {
             onMove?.Invoke(context.ReadValue<Vector2>());
         }
 
-        public void OnLook(InputValue value)
+        public void OnLook(InputAction.CallbackContext context)
         {
-            onLook?.Invoke(value.Get<Vector2>());
+            Debug.Log(UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId));
+            onLook?.Invoke(context.ReadValue<Vector2>());
         }
 
         public void OnInteract(InputValue value)
