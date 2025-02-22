ee276e3167a006d255c473b2d2a59f4bd6bfdbb9
omrkocar <kocartahir@gmail.com>
2025-02-22 15:58:05 -0500

    Updated CameraManager

--- Skyward_Project/Assets/Scripts/Gameplay/Player/CameraManager.cs
+++ Skyward_Project/Assets/Scripts/Gameplay/Player/CameraManager.cs
@@ -9,16 +9,29 @@ public class CameraManager : MonoBehaviour
     public CinemachineOrbitalFollow cam;
     
     private Vector2 lastInput;
+    private float lastX;
+    private float lastY;
 
     void Update()
     {
+        bool isUsingTouch = Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed;
+        if (isUsingTouch == false)
+            return;
+        
         Vector2 input = inputActionReference.action.ReadValue<Vector2>();
         
-        bool isUsingTouch = Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed;
         Vector2 movementDelta = input - lastInput;
-        if (isUsingTouch)
-            inputController.enabled = movementDelta.magnitude > float.Epsilon;
-
         lastInput = input;
+        
+        bool isStop = movementDelta.magnitude <= float.Epsilon;
+        if (!isStop)
+        {
+            lastX = cam.HorizontalAxis.Value;
+            lastY = cam.VerticalAxis.Value;
+            return;
+        }
+
+        cam.HorizontalAxis.Value = lastX;
+        cam.VerticalAxis.Value = lastY;
     }
 }
