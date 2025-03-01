fa40b4a2efcec530aea5be90bda8ca8a0e5d2e5b
celilbulut <celilbl12@gmail.com>
2025-02-28 21:03:07 -0500

    Menu and player ui changed

--- Skyward_Project/Assets/Scripts/UI/GameHUDComponent.cs
+++ Skyward_Project/Assets/Scripts/UI/GameHUDComponent.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameHUDComponent : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject pauseScreen;
+    public GameObject playerUIScreen;
+    public GameObject quitScreen;
+
+
+    private void Start()
     {
-        
+        OpenPlayerUIScreen();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenPauseScreen()
     {
-        
+        pauseScreen.SetActive(true);
+        playerUIScreen.SetActive(true);
+        quitScreen.SetActive(false);
+    }
+
+    public void OpenPlayerUIScreen()
+    {
+        pauseScreen.SetActive(false);
+        playerUIScreen.SetActive(true);
+        quitScreen.SetActive(false);
+    }
+
+    public void OpenQuitScreen()
+    {
+        pauseScreen.SetActive(true);
+        playerUIScreen.SetActive(false);
+        quitScreen.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
