811027fff9a6f3bc5d6710c98bb06c226837c104
omrkocar <kocartahir@gmail.com>
2025-03-15 11:11:18 -0400

    Renamed height conversion factor

--- Skyward_Project/Assets/Scripts/Configs/PlayerConfig.cs
+++ Skyward_Project/Assets/Scripts/Configs/PlayerConfig.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Skyward/Configs/Player Config", fileName = "PlayerConfig")]
 public class PlayerConfig : ScriptableObject
 {
-    public float heightIndicatorDivident = 1.5f;
+    public float heightConversionFactor = 1.5f;
 }
--- Skyward_Project/Assets/Scripts/Systems/HeightSystem.cs
+++ Skyward_Project/Assets/Scripts/Systems/HeightSystem.cs
@@ -23,7 +23,7 @@ public class HeightSystem : BaseSystem<HeightSystem>, ISkywardComponent
     {
         while (true)
         {
-            height = (player.position.y - startingY) / Configs.PlayerConfig.heightIndicatorDivident;
+            height = (player.position.y - startingY) / Configs.PlayerConfig.heightConversionFactor;
             yield return null;
         }
     }
