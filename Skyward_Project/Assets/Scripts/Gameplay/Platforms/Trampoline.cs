df864e00053489590bb75d17cbb2bcfbb10162bb
omrkocar <kocartahir@gmail.com>
2025-03-26 18:44:50 -0400

    cleanup

--- Skyward_Project/Assets/Scripts/Gameplay/Platforms/Trampoline.cs
+++ Skyward_Project/Assets/Scripts/Gameplay/Platforms/Trampoline.cs
@@ -7,27 +7,8 @@ public class Trampoline : Platform
 {
     public float force = 100f;
 
-    protected override IEnumerator Start()
-    {
-        yield return base.Start();
-        yield return new WaitUntil(() => PlayerSystem.Player != null);
-    }
-
     protected override IEnumerator OnPlayerLanded(PlayerController player)
     {
         yield return player.GetComponent<LocomotionController>().ForceJump(force);
-    }
-
-    private void OnApplyExternalForce(object sender, ExternalForceArgs e)
-    {
-        Vector3 force = ApplyForce(e.velocity);
-        e.velocity += force;
-    }
-
-    private Vector3 ApplyForce(Vector3 velocity)
-    {
-        if (!playerLanded)
-            return Vector3.zero;
-        return transform.up * force;
     }
 }
