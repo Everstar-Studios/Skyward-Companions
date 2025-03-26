606f7ead713b91f89929f2dd0133437e3f351d0e
omrkocar <kocartahir@gmail.com>
2025-03-26 09:36:04 -0400

    OpenMenuButtonAsset

--- Skyward_Project/Assets/Scripts/UI/UI Assets/OpenMenuButtonAsset.cs
+++ Skyward_Project/Assets/Scripts/UI/UI Assets/OpenMenuButtonAsset.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 namespace Skyward.UI
 {
+    [CreateAssetMenu(fileName = "OpenMenuButton", menuName = "Skyward/UI/Open Menu Button")]
     public class OpenMenuButtonAsset : ButtonAsset
     {
+        public MenuAsset menuAsset;
+        
         public override void Action()
         {
             base.Action();
+
+            UIGameMenu.ShowUI(menuAsset);
         }
     }
 
