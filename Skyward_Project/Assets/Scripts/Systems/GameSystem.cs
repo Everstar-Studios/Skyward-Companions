491a203217de81cf46f8266520a37823160cb483
omrkocar <kocartahir@gmail.com>
2025-03-27 06:21:06 -0400

    Unlocked scenes are now saved

--- Skyward_Project/Assets/Scripts/Systems/GameSystem.cs
+++ Skyward_Project/Assets/Scripts/Systems/GameSystem.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using Skyward.Core;
 using Skyward.Systems;
 using UnityEngine;
@@ -8,7 +10,8 @@ using UnityEngine.SceneManagement;
 public class GameSystem : BaseSystem<GameSystem>
 {
     private SkywardGame gameInstance;
-    
+
+    private SceneInfo sceneInfo = new();
     public static event EventHandler LevelCompleted
     {
         add => Instance.levelCompleted += value;
@@ -16,6 +19,14 @@ public class GameSystem : BaseSystem<GameSystem>
     }
 
     private event EventHandler levelCompleted;
+
+    protected override void Preload(GameContext context)
+    {
+        base.Preload(context);
+        
+        context.Store("Scene", sceneInfo);
+    }
+
     protected override void Initialize(GameContext context)
     {
         base.Initialize(context);
@@ -30,8 +41,70 @@ public class GameSystem : BaseSystem<GameSystem>
 
     public static void OnLevelCompleted()
     {
+        Instance.sceneInfo.MarkComplete(SceneManager.GetActiveScene());
         GameInputSystem.DisableInput();
         Instance.levelCompleted?.Invoke(Instance, EventArgs.Empty);
-        Instance.gameInstance.OnLevelCompleted();
+        MainMenu();
+    }
+
+    public static void Quit()
+    {
+        Instance.GameContext.game.Quit();
+    }
+    
+    public static void MainMenu()
+    {
+        Quit();
+        SceneManager.LoadScene("SCN_Lobby");
+    }
+
+    private class SceneInfo : ISkywardSerializable
+    {
+        private HashSet<string> unlockedLevels = new();
+        public void Serialize(BinaryWriter writer)
+        {
+            writer.Write(unlockedLevels.Count);
+            foreach (var level in unlockedLevels)
+                writer.Write(level);
+        }
+
+        public void Deserialize(BinaryReader reader)
+        {
+            int completedLevelCount = reader.ReadInt32();
+            for (int i = 0; i < completedLevelCount; i++)
+            {
+                string completedLevelName = reader.ReadString();
+                unlockedLevels.Add(completedLevelName);
+            }
+        }
+        
+        public void MarkComplete(Scene scene)
+        {
+            if (TryGetNextSceneName(out string nextSceneName))
+                unlockedLevels.Add(nextSceneName);
+        }
+        
+        private bool TryGetNextSceneName(out string nextSceneName)
+        {
+            int currentIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextIndex = currentIndex + 1;
+            nextSceneName = null;
+
+            if (nextIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                string path = SceneUtility.GetScenePathByBuildIndex(nextIndex);
+                nextSceneName = Path.GetFileNameWithoutExtension(path);
+                return true;
+            }
+
+            return false;
+        }
+        
+        public bool IsUnlocked(string sceneName) => unlockedLevels.Contains(sceneName);
+    }
+
+    public static bool IsLevelUnlocked(string sceneName)
+    {
+        return Instance.sceneInfo.IsUnlocked(sceneName);
     }
 }
--- Skyward_Project/Assets/Scripts/UI/LevelButton.cs
+++ Skyward_Project/Assets/Scripts/UI/LevelButton.cs
@@ -1,16 +1,72 @@
+using System;
+using Skyward.Core;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LevelButton : MonoBehaviour
+[RequireComponent(typeof(Button))]
+public class LevelButton : MonoBehaviour, ISkywardComponent
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string sceneName;
+    public bool unlockedByDefault = false;
+    private Button button;
+    public GameObject lockIcon;
+
+    private void Awake()
     {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
         
+        bool isUnlocked = unlockedByDefault || GameSystem.IsLevelUnlocked(sceneName);
+        button.interactable = isUnlocked;
+        lockIcon.SetActive(!isUnlocked);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnClick()
     {
-        
+        if (!unlockedByDefault && !GameSystem.IsLevelUnlocked(sceneName))
+            return;
+
+        GameSystem.LaunchLevel(sceneName);
     }
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(LevelButton))]
+public class LevelButtonEditor : Editor
+{
+    private string[] sceneNames;
+
+    private void OnEnable()
+    {
+        int sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        sceneNames = new string[sceneCount];
+
+        for (int i = 0; i < sceneCount; i++)
+        {
+            string path = UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(i);
+            sceneNames[i] = System.IO.Path.GetFileNameWithoutExtension(path);
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        LevelButton levelButton = (LevelButton)target;
+        SerializedProperty sceneNameProp = serializedObject.FindProperty("sceneName");
+
+        int currentIndex = System.Array.IndexOf(sceneNames, sceneNameProp.stringValue);
+        if (currentIndex < 0) currentIndex = 0;
+
+        int selectedIndex = EditorGUILayout.Popup("Scene Name", currentIndex, sceneNames);
+        sceneNameProp.stringValue = sceneNames[selectedIndex];
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("unlockedByDefault"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("lockIcon"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
