13b0332ebe699dd3054d51af507ca3aac37d36fa
celilbulut <celilbl12@gmail.com>
2025-05-06 19:57:52 -0400

    Muzik ses ayarlari yapildi ve slider calisiyor.

---
+++ Skyward_Project/Assets/Scripts/Sound/SettingsManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour
+{
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+
+    private void Start()
+    {
+        // Kayıtlı ayarları yükle veya varsayılanı kullan
+        float musicVol = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        float sfxVol = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        musicVolumeSlider.value = musicVol;
+        sfxVolumeSlider.value = sfxVol;
+
+        ApplyMusicVolume(musicVol);
+        ApplySFXVolume(sfxVol);
+
+        musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+        sfxVolumeSlider.onValueChanged.AddListener(OnSFXVolumeChanged);
+    }
+
+    private void OnMusicVolumeChanged(float value)
+    {
+        ApplyMusicVolume(value);
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    private void OnSFXVolumeChanged(float value)
+    {
+        ApplySFXVolume(value);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+
+    private void ApplyMusicVolume(float value)
+    {
+        AudioSystem.Instance.SetMusicVolume(value);
+    }
+
+    private void ApplySFXVolume(float value)
+    {
+        AudioSystem.Instance.SetSoundEffectsVolume(value);
+    }
+}
--- Skyward_Project/Assets/Scripts/Systems/AudioSystem.cs
+++ Skyward_Project/Assets/Scripts/Systems/AudioSystem.cs
@@ -1,47 +1,105 @@
-using System;
+using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections.Generic;
 using Skyward.Core;
-using UnityEngine;
 
 [RequiredSystem]
 public class AudioSystem : BaseSystem<AudioSystem>, ISkywardComponent
 {
-    private AudioSource audioSource;
+    [Header("Mixer")]
+    public AudioMixer audioMixer;
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("Sounds")]
+    public List<Sound> sounds; // Inspector'dan doldurulacak
+
+    public static float SoundEffectsVolume { get; private set; } = 1f;
+
+    private void Start()
+    {
+        Play("MainMenu");
+    }
 
     protected override void Awake()
     {
         base.Awake();
 
-        audioSource = gameObject.AddComponent<AudioSource>();
+        // if (musicSource == null || sfxSource == null)
+        // {
+        //     Debug.LogError("AudioSources not assigned in inspector!");
+        // }
     }
 
+    // Yeni sistem: isme göre oynat
+    public void Play(string name, float volume = 1f)
+    {
+        Sound sound = sounds.Find(s => s.name == name);
+        if (sound == null)
+        {
+            Debug.LogWarning("Sound not found: " + name);
+            return;
+        }
+
+        switch (sound.type)
+        {
+            case SoundType.Music:
+                musicSource.clip = sound.clip;
+                musicSource.loop = true;
+                musicSource.Play();
+                break;
+
+            case SoundType.SFX:
+                sfxSource.PlayOneShot(sound.clip, volume * SoundEffectsVolume);
+                break;
+        }
+    }
+
+    // Geriye dönük destek: doğrudan AudioClip oynat (SFX olarak)
     public static void Play(AudioClip clip, float volume = 1f)
     {
-        Instance.audioSource.PlayOneShot(clip, volume);
+        if (clip == null || Instance == null || Instance.sfxSource == null) return;
+        Instance.sfxSource.PlayOneShot(clip, volume * SoundEffectsVolume);
     }
 
+    // Geriye dönük destek: rastgele ses çal (SFX olarak)
     public static void PlayRandom(AudioClip[] clips)
     {
-        var clip = clips[UnityEngine.Random.Range(0, clips.Length)];
-        Play(clip, 1f);
-    }
-
-    public static void Stop()
-    {
-        Instance.audioSource.Stop();
+        if (clips == null || clips.Length == 0) return;
+        var clip = clips[Random.Range(0, clips.Length)];
+        Play(clip);
     }
 
     public static void Pause()
     {
-        Instance.audioSource.Pause();
-        foreach (var ambient in ComponentSystem.GetAllComponents<AmbientSoundPlayer>())
-            ambient.Pause();
+        Instance.musicSource?.Pause();
+        Instance.sfxSource?.Pause();
     }
-    
+
     public static void Unpause()
     {
-        Instance.audioSource.UnPause();
-        foreach (var ambient in ComponentSystem.GetAllComponents<AmbientSoundPlayer>())
-            ambient.Unpause();
+        Instance.musicSource?.UnPause();
+        Instance.sfxSource?.UnPause();
+    }
+
+    public void StopMusic()
+    {
+        if (musicSource.isPlaying)
+            musicSource.Stop();
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        float db = value <= 0.0001f ? -80f : Mathf.Log10(value) * 20f;
+        audioMixer.SetFloat("MusicVolume", db);
+    }
+
+    public void SetSoundEffectsVolume(float value)
+    {
+        SoundEffectsVolume = value;
+        float db = value <= 0.0001f ? -80f : Mathf.Log10(value) * 20f;
+        audioMixer.SetFloat("SFXVolume", db);
     }
 }
