6444eb31ac5707143e8098d488241feedf2be210
omrkocar <kocartahir@gmail.com>
2025-05-31 15:41:28 -0400

    Analytics System now initializes Firebase

---
+++ Skyward_Project/Assets/Scripts/Systems/AnalyticsSystem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using Firebase;
+using Skyward.Core;
+using UnityEngine;
+
+[RequiredSystem]
+public class AnalyticsSystem : BaseSystem
+{
+    protected override void Initialize(GameContext context)
+    {
+        base.Initialize(context);
+
+        StartCoroutine(InitializeFirebase());
+
+    }
+
+    private IEnumerator InitializeFirebase()
+    {
+        var handle = FirebaseApp.CheckAndFixDependenciesAsync();
+        yield return handle;
+        
+        if (handle.Result == DependencyStatus.Available)
+            Debug.Log("Firebase is successfully initialized.");
+        else
+            Debug.LogError($"Could not resolve Firebase dependencies: {handle.Result}");
+    }
+}
